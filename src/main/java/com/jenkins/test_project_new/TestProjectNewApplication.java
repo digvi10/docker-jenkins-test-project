edd76859c72511c07d9e849165782c36a3aeade0
Digvijay Singh <digvi10@gmail.com>
2024-02-28 19:34:40 +0530

    first Commit

---
+++ src/main/java/com/jenkins/test_project_new/TestProjectNewApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.test_project_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/test_project_new/TestProjectNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.test_project_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
